a8c05d9f47518352bceb304b13ce3bcfa680a76c
islam37 <islamuddin3725@gmail.com>
2025-09-18 19:47:45 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold text-center mt-5'> Hello this is course management system </h1>
+    </> 
+  )
+}
+
+export default App
